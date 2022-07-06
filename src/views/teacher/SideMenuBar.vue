e2d786a2bb32ea8e614d32a905b5df14a3e9fcab
th1nk <vinkangnet@gmail.com>
2022-07-06 21:45:30 +0800

    修改路由,建立admin与teacher初始页面

---
+++ src/views/teacher/SideMenuBar.vue
@@ -0,0 +1,86 @@
+<template>
+    <el-menu :default-active="activeMenu" class="side-menu" unique-opened @select="handelSelect">
+        <el-sub-menu index="questionManage">
+            <template #title>
+                <el-icon>
+                    <Notebook />
+                </el-icon>
+                <span>考题管理</span>
+            </template>
+            <el-menu-item index="questionTeacher">
+                <el-icon>
+                    <Reading />
+                </el-icon>
+                <span>查看考题</span>
+            </el-menu-item>
+            <el-menu-item index="questionTeacherAdd">
+                <el-icon>
+                    <Plus />
+                </el-icon>
+                <span>新增考题</span>
+            </el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="examManage">
+            <template #title>
+                <el-icon>
+                    <Document />
+                </el-icon>
+                <span>考试管理</span>
+            </template>
+            <el-menu-item index="examTeacher">
+                <el-icon>
+                    <DocumentChecked />
+                </el-icon>
+                <span>查看考试</span>
+            </el-menu-item>
+            <el-menu-item index="examTeacherAdd">
+                <el-icon>
+                    <DocumentAdd />
+                </el-icon>
+                <span>新增考试</span>
+            </el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="studentManage">
+            <template #title>
+                <el-icon>
+                    <User />
+                </el-icon>
+                <span>学生管理</span>
+            </template>
+            <el-menu-item index="studentTeacher">
+                <el-icon>
+                    <View />
+                </el-icon>
+                <span>查看学生</span>
+            </el-menu-item>
+            <el-menu-item index="studentTeacherAdd">
+                <el-icon>
+                    <Plus />
+                </el-icon>
+                <span>添加学生</span>
+            </el-menu-item>
+        </el-sub-menu>
+    </el-menu>
+</template>
+<script setup>
+import { Notebook, Reading, Plus, Document, DocumentChecked, DocumentAdd, User, View } from '@element-plus/icons-vue'
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+// 菜单默认激活项
+const activeMenu = ref('')
+// 菜单子项被点击
+const handelSelect = (index) => {
+    // 路由跳转
+    router.push({ name: index })
+}
+onMounted(() => {
+    // 从路由获取并设置默认激活页面
+    activeMenu.value = router.currentRoute.value.name
+})
+
+
+</script>
+<style>
+</style>
