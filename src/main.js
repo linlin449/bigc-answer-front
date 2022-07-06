cd84e94e10e9a4e46b0be779d9fea66f681ee224
th1nk <vinkangnet@gmail.com>
2022-07-06 18:06:30 +0800

    添加vue-router依赖,整理文件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import "lib-flexible/flexible.js"
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.use(ElementPlus)
+
+app.mount('#app')
