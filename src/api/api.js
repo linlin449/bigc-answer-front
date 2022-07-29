9b66c25e68b5fb04ae332da73e98662747c118f4
th1nk <vinkangnet@gmail.com>
2022-07-29 15:01:07 +0800

    增加页面: 收藏夹

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import service from '../util/service'
-
+import qs from 'qs'
 /**
  * 判断某题是否被学生收藏
  * @param {Number} questionId 问题ID
@@ -27,4 +27,56 @@ export const addFavoriteQuestion = (questionId, username) => {
  */
 export const deleteFavoriteQuestion = (questionId, username) => {
     return service.get(`/api/favorite/delete/${questionId}/username/${username}`)
+}
+
+/**
+ * 分页获取收藏题目列表
+ * @param {Number} page 页码 
+ * @param {String} username 学生username
+ * @returns 
+ */
+export const getFavoriteList = (page, username) => {
+    return service.get(`/api/favorite/list/${page}/username/${username}`)
+}
+
+/**
+ * 根据问题idList获取问题List
+ * @param {Number[]} idList 
+ */
+export const getQuestionListByIds = (idList) => {
+    return service.get(`/api/question/ids?${qs.stringify({ questionIds: idList }, { arrayFormat: "comma", encode: false })}`)
+}
+
+/**
+ * 根据问题ID获取问题详细
+ * @param {Number} questionId 问题ID
+ */
+export const getQuestionById = (questionId) => {
+    return service.get(`/api/question/${questionId}`)
+}
+
+/**
+ * 根据问题ID获取选项详细
+ * @param {Number} questionId 问题ID
+ */
+export const getOptionById = (questionId) => {
+    return service.get(`/api/option/question/${questionId}`)
+}
+
+/**
+ * 检查某学生对某问题的答题情况
+ * @param {Number} questionId 问题ID
+ * @param {String} username 学生username
+ */
+export const checkQuestionStatus = (questionId, username) => {
+    return service.get(`/api/question/check/${questionId}/username/${username}`)
+}
+
+/**
+ * 获取题目的正确答案
+ * @param {Number} questionId 
+ * @returns 
+ */
+export const getRightAnswerById = (questionId) => {
+    return service.get(`/api/rightanswer/question/${questionId}`)
 }
